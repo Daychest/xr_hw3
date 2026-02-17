86b385fb49f3928c95f7aee12a5702aa28d0e004
Daychest <veikka.paivarinta@gmail.com>
2026-02-17 13:03:29 +0200

    animation works badly

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -2,23 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(ActionBasedController))]
+
 public class HandController : MonoBehaviour
 {
-    ActionBasedController controller;
+    public InputActionReference gripAction;
+    public InputActionReference triggerAction;
     public Hand hand;
 
     // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<ActionBasedController>();
+        gripAction.action.Enable();
+        triggerAction.action.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        hand.SetGrip(controller.selectAction.action.ReadValue<float>());
-        hand.SetTrigger(controller.activateAction.action.ReadValue<float>());   
+        
+        hand.SetGrip(gripAction.action.ReadValue<float>());
+        hand.SetTrigger(triggerAction.action.ReadValue<float>());   
     }
 }
