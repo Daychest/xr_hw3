b3e662bbedb857d755a81c3f1fdcc07e700b4eb4
Daychest <veikka.paivarinta@gmail.com>
2026-02-23 23:48:18 +0200

    knife snapping works both hands

--- Assets/Scripts/CustomGrab.cs
+++ Assets/Scripts/CustomGrab.cs
@@ -17,8 +17,9 @@ public class CustomGrab : MonoBehaviour
     Vector3 lastPosition;
     Quaternion lastRotation;
 
-    public Vector3 testpos;
-    public Vector3 testrot;
+    public bool rightHand;
+    private Vector3 grabPositionCorrection = new Vector3(1, 1, 1);
+    private Vector3 grabRotationCorrection = new Vector3(0, 0, 0);
 
     private void Start()
     {
@@ -33,6 +34,12 @@ public class CustomGrab : MonoBehaviour
             if (c != this)
                 otherHand = c;
         }
+
+        if (rightHand)
+        {
+            grabPositionCorrection = new Vector3(-1, 1, 1);
+            grabRotationCorrection = new Vector3(0, 0, 180);
+        }
     }
 
     void Update()
@@ -44,10 +51,23 @@ public class CustomGrab : MonoBehaviour
             if (!grabbedObject)
             {
                 grabbedObject = nearObjects.Count > 0 ? nearObjects[0] : otherHand.grabbedObject;
-                grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
-                grabbedObject.GetComponent<Rigidbody>().detectCollisions = false;
+
+                Rigidbody rigidbody = grabbedObject.GetComponent<Rigidbody>();
+                if (rigidbody != null)
+                {
+                    rigidbody.isKinematic = true;
+                    rigidbody.detectCollisions = false;
+                }
 
                 Throwable throwable = grabbedObject.GetComponent<Throwable>();
+                if (throwable != null)
+                {
+                    grabbedObject.position = transform.position + transform.TransformDirection(Vector3.Scale(throwable.grabPositionOffset, grabPositionCorrection));
+
+                    grabbedObject.rotation = transform.rotation;
+                    grabbedObject.rotation *= Quaternion.Euler(grabRotationCorrection);
+                    grabbedObject.rotation *= Quaternion.Euler(throwable.grabRotationOffset);
+                }
             }
 
             if (grabbedObject)
@@ -55,24 +75,21 @@ public class CustomGrab : MonoBehaviour
                 // Change these to add the delta position and rotation instead
                 // Save the position and rotation at the end of Update function, so you can compare previous pos/rot to current here
 
-                if (false)
-                {
-                    Quaternion deltaRotation = transform.rotation * Quaternion.Inverse(lastRotation);
+                Quaternion deltaRotation = transform.rotation * Quaternion.Inverse(lastRotation);
 
-                    grabbedObject.rotation = deltaRotation * grabbedObject.rotation;
+                grabbedObject.rotation = deltaRotation * grabbedObject.rotation;
 
-                    Vector3 posDiff = lastPosition - grabbedObject.transform.position;
-                    posDiff = deltaRotation * posDiff;
-                    grabbedObject.transform.position = transform.position - posDiff;
-                }
+                Vector3 posDiff = lastPosition - grabbedObject.transform.position;
+                posDiff = deltaRotation * posDiff;
+                grabbedObject.transform.position = transform.position - posDiff;
 
                 Throwable throwable = grabbedObject.GetComponent<Throwable>();
                 if (throwable != null)
                 {
-                    grabbedObject.position = transform.position + transform.TransformDirection(Vector3.Scale(throwable.grabPositionOffset, testpos));
+                    grabbedObject.position = transform.position + transform.TransformDirection(Vector3.Scale(throwable.grabPositionOffset, grabPositionCorrection));
 
                     grabbedObject.rotation = transform.rotation;
-                    grabbedObject.rotation *= Quaternion.Euler(testrot);
+                    grabbedObject.rotation *= Quaternion.Euler(grabRotationCorrection);
                     grabbedObject.rotation *= Quaternion.Euler(throwable.grabRotationOffset);
                 }
             }
@@ -80,8 +97,12 @@ public class CustomGrab : MonoBehaviour
         // If let go of button, release object
         else if (grabbedObject)
         {
-            grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
-            grabbedObject.GetComponent<Rigidbody>().detectCollisions = true;
+            Rigidbody rigidbody = grabbedObject.GetComponent<Rigidbody>();
+            if (rigidbody != null)
+            {
+                rigidbody.isKinematic = false;
+                rigidbody.detectCollisions = true;
+            }
             grabbedObject = null;
         }
 
